15df762676d1587cd78a40fd52f24361d09ee135
skander <skander.tlili@enstab.ucar.tn>
2024-03-30 22:04:20 +0100

    creating another endpoint colled test endpoint returning text response in a new url : /test

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+@app.get("/test")
+def test_endpoint():
+    return "hello this is a test"
+
+
 @app.get("/")
 def home():
     return "Hello World"
