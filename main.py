16153d61a3c11a6364bffb51462798dbc60f3f82
skander <skander.tlili@enstab.ucar.tn>
2024-03-31 21:56:20 +0100

    creating another user instance that the endpoint redirects its info in another url containing the user id

--- main.py
+++ main.py
@@ -28,19 +28,35 @@ class FullUserProfile(User):
 
 
 
-def get_user_info() -> FullUserProfile:
+def get_user_info(user_id : str = "default") -> FullUserProfile:
 
-    profile_info = {
-        "short_description": "My bio description",
-        "long_bio" : "This is our longer bio",
+    profile_infos = {
+        "default" : {
+            "short_description": "My bio description",
+            "long_bio" : "This is our longer bio",
+        },
+        "user_1":{
+            "short_description": "user 1's description",
+            "long_bio" : "user 1's longer bio",
+
+        }        
     }
     
+    profile_info = profile_infos[user_id]
 
-    user_content = {
-        "liked_posts" : [1]*3,
-        "profile_info" : profile_info
+    users_content = {
+
+        "default" : {
+            "liked_posts" : [1]*3,
+            "profile_info" : profile_infos["default"]
+        },
+        "user_1" : {
+            "liked_posts" : [],
+            "profile_info" : profile_info             
+        }
     }
-
+          
+    user_content = users_content[user_id]
     user = User(**user_content)
 
     fulluserprofile = {
@@ -57,6 +73,17 @@ def test_endpoint():
     user = get_user_info()
 
     return user
+
+
+
+@app.get("/user/{user_id}", response_model=FullUserProfile)
+def test_endpoint_by_id(user_id : str):
+
+    user = get_user_info(user_id)
+
+    return user
+
+
     
 
 
