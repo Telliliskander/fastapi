592342c81f5828949be97d0fdd105ce24f84ff62
skander <skander.tlili@enstab.ucar.tn>
2024-03-31 02:18:33 +0100

    adding some oop & typing changes

--- main.py
+++ main.py
@@ -1,27 +1,36 @@
 from fastapi import FastAPI
 from fastapi.responses import PlainTextResponse, JSONResponse
 from pydantic import BaseModel
+from typing import Optional
+
+
 
 app = FastAPI()
 
+
+
 class User(BaseModel):
     username : str
-    description : str
+    description : str = "My bio description"
+    liked_posts : Optional[list[int]]=None
 
-def get_user_info() -> list[str, str]:
 
-    username = "testuser"
-    short_description = "my bio"
 
-    return username, short_description
+def get_user_info() -> User:
+
+    content = {
+        "username" : "testuser",
+        # "description" : "My bio description",
+        # "liked_posts" : None
+    }
+
+    return User(**content)
 
 
 @app.get("/user/me", response_model=User)
 def test_endpoint():
 
-    username, short_description = get_user_info()
-
-    user = User(username=username, description = short_description)
+    user = get_user_info()
 
     return user
     
