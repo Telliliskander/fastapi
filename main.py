d48431a2ea1bbcc8402a0c0bd55db00c27fe7681
skander <skander.tlili@enstab.ucar.tn>
2024-04-01 06:06:09 +0100

    creating post endpoint to create user

--- main.py
+++ main.py
@@ -4,8 +4,35 @@ from pydantic import BaseModel, Field
 from typing import Optional
 
 
+
+
 app = FastAPI()
 
+# ------------------- Variables ---------------------------------
+
+profile_infos = {
+    0 : {
+        "short_description": "My bio description",
+        "long_bio" : "This is our longer bio",
+    },
+}        
+
+
+users_content = {
+
+    0 : {
+        "liked_posts" : [1]*3,
+    },
+}
+
+
+
+
+
+
+
+
+#--------------------- Classes ----------------------------------
 
 class User(BaseModel):
 
@@ -27,36 +54,19 @@ class FullUserProfile(User):
     long_bio : str
 
 
+class CreateUserResponse(BaseModel):
+    user_id : int
 
-def get_user_info(user_id : str = "default") -> FullUserProfile:
 
-    profile_infos = {
-        "default" : {
-            "short_description": "My bio description",
-            "long_bio" : "This is our longer bio",
-        },
-        "user_1":{
-            "short_description": "user 1's description",
-            "long_bio" : "user 1's longer bio",
 
-        }        
-    }
-    
-    profile_info = profile_infos[user_id]
 
-    users_content = {
+#--------------------- Functions ---------------------------------
 
-        "default" : {
-            "liked_posts" : [1]*3,
-            "profile_info" : profile_infos["default"]
-        },
-        "user_1" : {
-            "liked_posts" : [],
-            "profile_info" : profile_info             
-        }
-    }
-          
+def get_user_info(user_id : int = 0) -> FullUserProfile:
+
+    profile_info = profile_infos[user_id]      
     user_content = users_content[user_id]
+    
     user = User(**user_content)
 
     fulluserprofile = {
@@ -67,6 +77,31 @@ def get_user_info(user_id : str = "default") -> FullUserProfile:
     return FullUserProfile(**fulluserprofile)
 
 
+def create_user(full_profile_info : FullUserProfile) -> int:
+
+    global profile_infos
+    global users_content
+
+    new_user_id = len(profile_infos)
+    liked_posts = full_profile_info.liked_posts
+    short_description = full_profile_info.short_description
+    long_bio = full_profile_info.long_bio 
+
+    users_content[new_user_id] = {"liked_posts" : liked_posts}
+    profile_infos[new_user_id] = {
+        "short_description" : short_description,
+        "long_bio" : long_bio
+        }
+
+    return new_user_id
+
+
+
+
+
+
+
+# --------------------- Endpoints ---------------------------------
 @app.get("/user/me", response_model=FullUserProfile)
 def test_endpoint():
 
@@ -77,15 +112,17 @@ def test_endpoint():
 
 
 @app.get("/user/{user_id}", response_model=FullUserProfile)
-def test_endpoint_by_id(user_id : str):
+def test_endpoint_by_id(user_id : int):
 
     user = get_user_info(user_id)
 
     return user
 
 
-    
-
+@app.post("/users", response_model=CreateUserResponse)
+def add_user(full_profile_info : FullUserProfile):
+    created_user_id = create_user(full_profile_info)
+    return CreateUserResponse(user_id=created_user_id)
 
 @app.get("/")
 def home():
