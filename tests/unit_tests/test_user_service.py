0bb854bcb1eac4a649b52b10ecdc499f40146fc1
skander <skander.tlili@enstab.ucar.tn>
2024-04-17 11:24:30 +0100

    Testing for Exceptions

--- tests/unit_tests/test_user_service.py
+++ tests/unit_tests/test_user_service.py
@@ -1,5 +1,5 @@
 import pytest
-
+from app.services.exceptions import UserNotFound
 
 
 
@@ -10,4 +10,9 @@ async def test_delete_user_works_properly(user_service, valid_user_delete_id):
     assert valid_user_delete_id not in user_service.profile_infos
     assert valid_user_delete_id not in user_service.users_content
 
+@pytest.mark.asyncio 
+async def test_delete_invalid_user_raises_proper_exception(user_service, invalid_user_delete_id):
+    with pytest.raises(Exception):
 
+        await user_service.delete_user(invalid_user_delete_id)
+        
