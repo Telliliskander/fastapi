74bf865565e80c0ec8341bc1ac36e678054e624d
skander <skander.tlili@enstab.ucar.tn>
2024-04-18 08:02:06 +0100

    Testing sync requests

--- sample_requests/sync_requests.py
+++ sample_requests/sync_requests.py
@@ -1,18 +1,12 @@
 import requests
 import json
 
-response = requests.get("http://127.0.0.1:8000/user/0", headers={})
-print(response)
 
-sample_data_to_send = {
-    "name" : "bob",
-    "liked_posts" : [1,2,3,4],
-    "short_description" : "some short description", 
-    "long_bio" : "some long bio"
-}
-json_data = json.dumps(sample_data_to_send)
-print("json data : ", json_data)
-post_response = requests.post("http://127.0.0.1:8000/user/", headers={}, data=sample_data_to_send)
-print("post request response : ", post_response.json())
+def get_and_parse_user(base_url : str, endpoint_prefix : str, user_id : int):
+    
+    url = base_url + endpoint_prefix + str(user_id)
 
-requests.delete()
+    
+    response = requests.get(url)
+    post_response = requests.post(url)
+    return response.json()
