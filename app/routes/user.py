44d44aba83c2199b5de9b4da64fbfae027aad628
skander <skander.tlili@enstab.ucar.tn>
2024-10-18 19:59:28 +0100

    API Routers Continued : Adding prefix and tags to the router

--- app/routes/user.py
+++ app/routes/user.py
@@ -12,18 +12,19 @@ from app.services.user import UserService
 
 def create_user_router() -> APIRouter:
 
-    user_router = APIRouter()
+    user_router = APIRouter(
+        prefix = "/user",
+        tags = ["user"],
+        )
     user_service = UserService()
 
-    @user_router.get("/user/me", response_model=FullUserProfile)
-    async def test_endpoint():
+    @user_router.get("/all", response_model=MultipleUsersResponse)
+    async def get_all_users_paginated(start : int = 0, limit : int = 2):
+        users, total = await user_service.get_all_users_with_pagination(start, limit)
+        formatted_users = MultipleUsersResponse(users=users, total=total)
+        return formatted_users
 
-        full_user_profile = await user_service.get_user_info()
-
-        return full_user_profile
-
-
-    @user_router.get("/user/{user_id}", response_model=FullUserProfile)
+    @user_router.get("/{user_id}", response_model=FullUserProfile)
     async def get_user_by_id(user_id : int):
 
         full_user_profile = await user_service.get_user_info(user_id)
@@ -31,22 +32,15 @@ def create_user_router() -> APIRouter:
         return full_user_profile
 
 
-    @user_router.get("/users", response_model=MultipleUsersResponse)
-    async def get_all_users_paginated(start : int = 0, limit : int = 2):
-        users, total = await user_service.get_all_users_with_pagination(start, limit)
-        formatted_users = MultipleUsersResponse(users=users, total=total)
-        return formatted_users
 
-
-
-    @user_router.delete("/user/{user_id}")
+    @user_router.delete("/{user_id}")
     async def remove_user(user_id : int) -> None:
 
         await user_service.delete_user(user_id)
 
 
 
-    @user_router.put("/user/{user_id}")
+    @user_router.put("/{user_id}")
     async def update_user(user_id : int, full_profile_info : FullUserProfile):
 
         await user_service.create_update_user(full_profile_info, user_id)
@@ -54,7 +48,7 @@ def create_user_router() -> APIRouter:
         return None
 
 
-    @user_router.post("/users", response_model=CreateUserResponse)
+    @user_router.post("/", response_model=CreateUserResponse)
     async def add_user(full_profile_info : FullUserProfile):
         user_id = await user_service.create_update_user(full_profile_info)
         created_user = CreateUserResponse(user_id=user_id)
