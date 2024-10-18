eecac9aeb05a112bd3ab5e30baaf58b4db6bcf8f
skander <skander.tlili@enstab.ucar.tn>
2024-10-18 21:01:52 +0100

    HTTP Status Codes : Adding 201 status code to post endpoint

--- app/routes/user.py
+++ app/routes/user.py
@@ -48,7 +48,7 @@ def create_user_router() -> APIRouter:
         return None
 
 
-    @user_router.post("/", response_model=CreateUserResponse)
+    @user_router.post("/", response_model=CreateUserResponse, status_code = 201)
     async def add_user(full_profile_info : FullUserProfile):
         user_id = await user_service.create_update_user(full_profile_info)
         created_user = CreateUserResponse(user_id=user_id)
