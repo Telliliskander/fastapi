21eb1b4582d643933b3ba6ef0d915445c90a32ed
skander <skander.tlili@enstab.ucar.tn>
2024-11-02 21:40:35 +0100

    Fixtures 3 : reinitializing fixtures once for each test module (scope level = module)

--- tests/conftest.py
+++ tests/conftest.py
@@ -25,7 +25,7 @@ def _users_content():
 #     user_service  = UserService(_profile_infos, _users_content)
 #     return user_service
 
-@pytest.fixture(scope="class")
+@pytest.fixture(scope="module")
 def testing_fixture():
     print("Initializing fixture")
     return   "a" 
