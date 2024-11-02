639a04ef18a50d493b4b4f5491bcabff0ec4ac98
skander <skander.tlili@enstab.ucar.tn>
2024-11-02 21:32:13 +0100

    Fixtures 1 : reinitializing fixtures for each test function

---
+++ tests/test_1.py
@@ -0,0 +1,7 @@
+def test_1(testing_fixture):
+    print("##############testing_fixture :", testing_fixture)
+    assert False
+
+def test_2(testing_fixture):
+    print("#######testing_fixture :", testing_fixture)
+    assert False
